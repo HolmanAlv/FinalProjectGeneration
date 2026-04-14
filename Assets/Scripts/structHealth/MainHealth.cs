29d2c0e95c942583a3339f3923637811644ef4b1
Holman Alvarado <alvholman@gmail.com>
2026-04-14 10:57:37 -0500

    Add: Structure health

---
+++ Assets/Scripts/structHealth/MainHealth.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class MainHealth : MonoBehaviour
+{
+    public float maxHealth = 100f;
+    private float currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+
+        Debug.Log("Vida global: " + currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        Debug.Log("GAME OVER");
+    }
+}
